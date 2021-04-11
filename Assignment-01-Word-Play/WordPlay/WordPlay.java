63023db389acbd4d0d4701fa7189ca1e518d4049
Subutti0503 <subutti0503@gmail.com>
2021-04-11 16:34:25 +0900

    create assignment-02

--- Assignment-01-Word-Play/WordPlay/WordPlay.java
+++ Assignment-01-Word-Play/WordPlay/WordPlay.java
@@ -35,5 +35,81 @@ public class WordPlay {
 		boolean result4 = isVowel(ch4);
 		System.out.println(result4);
 	}
+	
+	
+	
+	
+	
+	public String replaceVowels(String phrase, char ch) {
+		
+		StringBuilder phraseRe = new StringBuilder(phrase);
+		
+		for(int i = 0; i < phraseRe.length(); i++) {
+			
+			char currChar = phraseRe.charAt(i);
+			
+			if(isVowel(currChar)) {
+				
+				phraseRe.setCharAt(i, ch);
+			}	
+		}
+		
+		return phraseRe.toString();
+	}
+	
+	
+	public void testreplaceVowels() {
+		
+		String result = replaceVowels("Hello World", '*');
+		
+		System.out.println(result);
+			
+	}
+	
+	
+	
+	
+	
+	public String emphasize (String phrase, char ch) {
+		
+		StringBuilder phraseRe = new StringBuilder(phrase);
+		
+	    for(int i = 0; i < phraseRe.length(); i++) {
+			
+			char currChar = phraseRe.charAt(i);
+			
+		    if(currChar == ch) {
+				
+			    if(i%2 == 0){
+					
+					// an odd number location
+					phraseRe.setCharAt(i, '*');
+			
+			    } else {
+					
+					phraseRe.setCharAt(i, '+');				
+					
+				}
+			
+			}
+			
+			// Do nothing
+	    }
+		
+		return phraseRe.toString();
+	}
+	
+	public void testemphasize () {
+			
+		String result;
+		
+		result = emphasize("dna ctgaaactga", 'a');
+		System.out.println(result);
+		// expected result: "dn* ctg+*+ctg+"
+			
+		result = emphasize("Mary Bella Abracadabra", 'a');	
+		System.out.println(result);	
+		// expected result: "M+ry Bell+ +br*c*d*br+
+	}
 }
 	
---
+++ Assignment-02-Caesar-Cipher/CaesarCipher.java
@@ -0,0 +1,5 @@
+import edu.duke.*;
+
+public class CaesarCipher {
+
+}
