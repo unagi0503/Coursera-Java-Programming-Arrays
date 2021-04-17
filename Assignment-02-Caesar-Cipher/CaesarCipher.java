86c6a4ae448db75668d6bc7083705b425fbe13ef
subutti0503 <subutti0503@gmail.com>
2021-04-17 13:45:17 +0900

    assingment 02 completed

--- Assignment-02-Caesar-Cipher/CaesarCipher.java
+++ Assignment-02-Caesar-Cipher/CaesarCipher.java
@@ -26,7 +26,7 @@ public class CaesarCipher {
                     char newChar = shiftedAlphabetUpper.charAt(idx);
                     //Replace the ith character of encrypted with newChar
                     encrypted.setCharAt(i, newChar);
-                }
+				}
                 //Otherwise: do nothing 		    
 			} else if(Character.isLowerCase(currChar)) {
 			    //Find the index of currChar in the alphabet (call it idx)
@@ -37,17 +37,17 @@ public class CaesarCipher {
                     char newChar = shiftedAlphabetLower.charAt(idx);
                     //Replace the ith character of encrypted with newChar
                     encrypted.setCharAt(i, newChar);
-                }
+				}
                 //Otherwise: do nothing 		    			
 			} else {
-			
+				
 			}
-		            
-        }
+			
+		}
 		
         //Your answer is the String inside of encrypted
         return encrypted.toString();
-    }
+	}
 	
 	public void testCaesar() {
         int key = 17;
@@ -61,16 +61,102 @@ public class CaesarCipher {
 		
 		String decrypted = encrypt(encrypted, 26-key);
         System.out.println(decrypted);
-    }
+	}
 	
 	
 	
 	
 	public String encryptTwoKeys(String input, int key1, int key2) {
 		
+		StringBuilder encrypted = new StringBuilder(input);
 		
-	
-	
+		String alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String alphabetLower = "abcdefghijklmnopqrstuvwxyz";
+		
+		String shiftedAlphabetUpperKey01 = alphabetUpper.substring(key1)+alphabetUpper.substring(0,key1);
+		String shiftedAlphabetLowerKey01 = alphabetLower.substring(key1)+alphabetLower.substring(0,key1);
+		
+		String shiftedAlphabetUpperKey02 = alphabetUpper.substring(key2)+alphabetUpper.substring(0,key2);
+		String shiftedAlphabetLowerKey02 = alphabetLower.substring(key2)+alphabetLower.substring(0,key2);
+		
+		char newChar;
+		
+		for(int i = 0; i < encrypted.length(); i++) {
+            //Look at the ith character of encrypted (call it currChar)
+			
+						
+			char currChar = encrypted.charAt(i);
+						
+			if (i%2 == 0) {
+				
+				if(Character.isUpperCase(currChar)) {
+				
+				    int idx = alphabetUpper.indexOf(currChar);
+					
+					if(idx != -1){
+					    newChar = shiftedAlphabetUpperKey01.charAt(idx);
+						encrypted.setCharAt(i, newChar);
+					}
+					
+				} else if(Character.isLowerCase(currChar)) {
+				
+				    int idx = alphabetLower.indexOf(currChar);
+					
+					if(idx != -1){
+					    newChar = shiftedAlphabetLowerKey01.charAt(idx);
+						encrypted.setCharAt(i, newChar);
+					}
+				
+				} else {
+				    // Do nothing
+				}
+				
+			} else {
+			
+			    if(Character.isUpperCase(currChar)) {
+				
+				    int idx = alphabetUpper.indexOf(currChar);
+					
+					if(idx != -1){
+					    newChar = shiftedAlphabetUpperKey02.charAt(idx);
+						encrypted.setCharAt(i, newChar);
+					}
+					
+				} else if(Character.isLowerCase(currChar)) {
+				
+				    int idx = alphabetLower.indexOf(currChar);
+					
+					if(idx != -1){
+					    newChar = shiftedAlphabetLowerKey02.charAt(idx);
+						encrypted.setCharAt(i, newChar);
+					}
+				
+				} else {
+				    // Do nothing
+				}
+			  
+			}
+						
+		}
+		
+		//Your answer is the String inside of encrypted
+        return encrypted.toString();
+			
 	}
-
+	
+	public void testEncryptTwoKeys() {
+        int key01 = 23;
+		int key02 = 17;
+        
+		FileResource fr = new FileResource();
+	    String message = fr.asString();
+		
+		String encrypted = encryptTwoKeys(message, key01, key02);
+        System.out.println(encrypted);
+		// expected result : "CFOPQ IBDFLK XQQXZH BXPQ CIXKH!"
+		
+		String decrypted = encryptTwoKeys(encrypted, 26-key01, 26-key02);
+        System.out.println(decrypted);
+	}
+	
 }
